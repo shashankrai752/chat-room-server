a055d33a1a8b2c7542d6080bf110e507eb052a12
shashankrai-cse <iamshashankrai@gmail.com>
2026-07-23 22:44:08 +0530

    prod ready

--- server.js
+++ server.js
@@ -41,8 +41,6 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
         console.log(`User disconnected: ${socket.id}`);
-        socket.leave(ROOM);
-        io.emit('roomNotice', `${username} left`); 
     });
 });
 
