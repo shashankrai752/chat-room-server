4ff88fbbe15a55725874232a1954404caa05f089
shashankrai-cse <iamshashankrai@gmail.com>
2026-07-23 21:29:29 +0530

    prod ready

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import http from "http";
 import {Server} from "socket.io";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 const server = http.createServer(app);
